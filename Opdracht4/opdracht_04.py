2b1700949b3b4235c699f242374d13d7f524b131
rlahoua7 <2152116@talnet.nl>
2021-11-10 12:49:09 +0100

    Rekenmachiene

--- Opdracht4/opdracht_04.py
+++ Opdracht4/opdracht_04.py
@@ -1,6 +1,26 @@
-age = 
+print("select an operationto perform: ")
+print("1. ADD")
+print("2. SUBTRSCT")
+print("3. MULTIPLY")
+print("4. DIVIDE")
 
-if conditie:
-    print(True)
+operation = input()
+
+if operation == "1":
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number: ")
+    print("The answer is " + str(int(num1) + int(num2)))
+elif operation == "2":
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number: ")
+    print("The answer is " + str(int(num1) - int(num2)))
+elif operation == "3":
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number: ")
+    print("The answer is " + str(int(num1) * int(num2)))
+elif operation == "4":
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number: ")
+    print("The answer is " + str(int(num1) / int(num2)))
 else:
-    print(False)
+    print("Invalid Entry")      
